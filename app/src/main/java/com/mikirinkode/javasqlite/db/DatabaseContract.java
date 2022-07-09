512b87969b3c4a3a8f66a3e581ae42ddb751c019
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-07-09 22:00:45 +0700

    intial commit: project sqlite

---
+++ app/src/main/java/com/mikirinkode/javasqlite/db/DatabaseContract.java
@@ -0,0 +1,12 @@
+package com.mikirinkode.javasqlite.db;
+
+import android.provider.BaseColumns;
+
+public class DatabaseContract {
+    static String TABLE_NAME = "note";
+    public static final class NoteColumns implements BaseColumns {
+        public static String TITLE = "title";
+        public static String DESCRIPTION = "description";
+        public static String DATE = "date";
+    }
+}
